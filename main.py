8e6d955bb8dd57124c546677f4db62afc271fc8e
lmfrozza <frozzalucca@gmail.com>
2023-11-10 23:29:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import time
 import keyboard
 import PySimpleGUI as sg
 
-
+velocidades = ["1 CPS","2 CPS","3 CPS","4 CPS","5 CPS","6 CPS","7 CPS","8 CPS"]
 global interval
-def click():
-    pyautogui.click()
+
+#def click():
+    #pyautogui.click()
 
 def main(val):
     # Intervalo entre cliques, em segundos
@@ -18,19 +19,54 @@ def main(val):
         if keyboard.is_pressed("q"):
             break
 
-        click()
+        pyautogui.click()
+        #click()
         time.sleep(float(val))
 
 layout =[
     [sg.Text("Selecione o intervalo:")],
-    [sg.InputText(key="intervalo", size= 10), sg.Button("iniciar")],
+    [sg.Combo(velocidades ,key="intervalo", size= 10), sg.Button("iniciar")],
 ]
-janela=sg.Window("3x3", layout)
 
+janela=sg.Window("AutoClicker", layout)
+ 
 while True:
     evento, valor= janela.read()
     if evento==sg.WIN_CLOSED:
         break
     if evento == "iniciar":
-        interval = valor["intervalo"]
+        if valor["intervalo"] == "1 CPS":
+            interval = 1
+            print("1")
+        if valor["intervalo"] == "2 CPS":
+            interval = 0.5
+            print("2")
+        if valor["intervalo"] == "3 CPS":
+            interval = 0.25
+            print("3")
+        if valor["intervalo"] == "4 CPS":
+            interval = 0.125
+            print("4")
+        if valor["intervalo"] == "5 CPS":
+            interval = 0.0625
+            print("5")
+        if valor["intervalo"] == "6 CPS":
+            interval = 0.03125
+            print("6")
+        if valor["intervalo"] == "7 CPS":
+            interval = 0.015625
+            print("7")
+        if valor["intervalo"] == "8 CPS":
+            interval = 0.0078125
+            print("8")
         main(interval)
+
+#1 = 1CPS
+#0.5 = 2CPS
+#0.25 = 3CPS 
+#0.125 = 4CPS
+#0.0625 = 5CPS
+#0.03125 = 6CPS
+#0.015625 = 7CPS
+#0.0078125 = 8CPS
+#0.00390625 = 9CPS
